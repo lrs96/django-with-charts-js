edf0c4e9f4f65883da97cda6e6c35ac5b198f444
Luan Rodrigues <luanlrs96@gmail.com>
2020-04-15 11:59:12 -0300

    Criação do model Skills

--- src/skills/models.py
+++ src/skills/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from profiles.models import Profile
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
+
+class Skill(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    score = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{}--{}--{}".format(self.user, self.name, self.score)
